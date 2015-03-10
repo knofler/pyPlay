a4194cf8908178294f169e213ba074a24675da8b
Rumman <ahmed.rumman@gmail.com>
2015-03-10 18:02:59 +1100

    is_prime added

--- learn.py
+++ learn.py
@@ -1,3 +1,29 @@
-number = range(10)
+number = range(51)
 
-print number
+comprehension = [i for i in number if i%2==0] 
+slicing 	  = number[1:len(number):2]
+
+print comprehension
+print slicing
+
+def is_prime(x):
+    char = range(x)[2:]
+
+    if x < 2:
+        return False 
+
+    elif x == 2:
+        print True
+        return True
+
+    else:   
+        for n in char:
+            if (x % n == 0):
+                print n
+                return False
+        else: 
+            print True
+            return True 
+# note the else statement is still inside the other else statement though
+
+is_prime(11)
